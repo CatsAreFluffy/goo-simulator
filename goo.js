f4213f5734f0daf42996d58464b28940e422a3d5
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2018-05-26 22:10:59 -0500

    Update goo.js

--- goo.js
+++ goo.js
@@ -14,7 +14,15 @@ let getMatter = () => {
 let buyNanite = () => {
 	if(matter.usable >= 10) {
 		matter.usable-=10;
-		nanites++
+		nanites++;
+	}
+	updateMatter();
+	updateNanomachines();
+};
+let sellNanite = () => {
+	if(nanites > 0) {
+		matter.usable += 10;
+		nanites--;
 	}
 	updateMatter();
 	updateNanomachines();
@@ -24,7 +32,11 @@ let updateMatter = () => {
 	document.getElementById('available matter').innerHTML = "Available matter:<br/>" + matter.available;
 };
 let updateNanomachines = () => {
-	document.getElementById('nanites').innerHTML = nanites + " nanites";
+	if(nanites === 1) {
+		document.getElementById('nanites').innerHTML = nanites + " nanite";
+	} else{
+		document.getElementById('nanites').innerHTML = nanites + " nanites";
+	}
 };
 let makeStuff = () => {
 	if(matter.available > nanites) {
@@ -34,8 +46,25 @@ let makeStuff = () => {
 		matter.usable += matter.available;
 		matter.available = 0;
 	}
-}
+};
+let unlocks = () => {
+	if(nanites > 100) {
+		let scouts = document.createElement("div");
+		let buy = document.createElement("button");
+		let sell = document.createElement("button");
+		let amount = document.createElement("p");
+		buy.onclick = "buyScout()";
+		sell.onclick = "sellScout()";
+		amount.innerHTML = "0 scouts";
+		amount.id = "scouts";
+		scouts.appendChild(buy);
+		scouts.appendChild(sell);
+		scouts.appendChild(amount);
+		document.getElementById('nanomachines').appendChild(scouts);
+	}
+};
 const ticker = () => {
 	makeStuff();
 	updateMatter();
+	unlocks();
 };
