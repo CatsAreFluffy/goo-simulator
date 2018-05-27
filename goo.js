d5a3da0823986204a043a4fc86950185a58efc58
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2018-05-26 21:45:20 -0500

    Made matter and nanites

--- goo.js
+++ goo.js
@@ -1,10 +1,41 @@
 let matter = {
-	available:100,
+	available:10000,
 	usable:0
 };
+let nanites = 0;
+let getMatter = () => {
+	if(matter.available > 0) {
+		matter.available--;
+		matter.usable++;
+	}
+	updateMatter();
+};
 
-
-
+let buyNanite = () => {
+	if(matter.usable <= 10) {
+		matter.usable-=10;
+		nanites++
+	}
+	updateMatter();
+	updateNanomachines();
+};
+let updateMatter = () => {
+	document.getElementById('usable matter').innerHTML = "Usable matter:<br/>" + matter.usable;
+	document.getElementById('available matter').innerHTML = "Available matter:<br/>" + matter.available;
+};
+let updateNanomachines = () => {
+	document.getElementById('nanites').innerHTML = nanites + "nanites";
+};
+let makeStuff = () => {
+	if(matter.available > nanites) {
+		matter.available -= nanites;
+		matter.usable += nanites;
+	} else{
+		matter.usable += matter.available;
+		matter.available = 0;
+	}
+}
 const ticker = () => {
-	updateStuff();
+	makeStuff();
+	updateMatter();
 };
