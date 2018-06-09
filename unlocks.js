7cb8c3ce41b8be3513b9ed9790cac401faadaa85
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2018-06-09 14:17:53 -0500

    Create unlocks.js

---
+++ unlocks.js
@@ -0,0 +1,41 @@
+let scoutsUnlock = true;
+let nanofactoryUnlock = true;
+const unlocks = () => {
+	if(nanites > 100 && scoutsUnlock === true) {
+		let scoutDiv = document.createElement("div");
+		let scoutBuy = document.createElement("button");
+		let scoutSell = document.createElement("button");
+		let amount = document.createElement("p");
+		scoutBuy.onclick = buyScout;
+		scoutBuy.innerHTML = "Create scout (Cost: 20 nanites)";
+		scoutSell.onclick = sellScout;
+		scoutSell.innerHTML = "Destroy scout";
+		
+		amount.innerHTML = "0 scouts";
+		amount.id = "scouts";
+		scoutDiv.appendChild(scoutBuy);
+		scoutDiv.appendChild(scoutSell);
+		scoutDiv.appendChild(amount);
+		document.getElementById('nanomachines').appendChild(scoutDiv);
+		scoutsUnlock = false;
+	}
+	if(nanites > 150 && nanofactoryUnlock === true) {
+		let nanofactoryDiv = document.createElement("div");
+		let nanofactoryBuy = document.createElement("button");
+		let nanofactorySell = document.createElement("button");
+		let nanofactoryAmount = document.createElement("p");
+		nanofactoryBuy.onclick = buyNanofactory;
+		nanofactoryBuy.innerHTML = "Create nanofactory (Cost: 50 nanites)";
+		nanofactorySell.onclick = sellNanofactory;
+		nanofactorySell.innerHTML = "Destroy nanofactory";
+		
+		nanofactoryAmount.innerHTML = "0 nanofactories";
+		nanofactoryAmount.id = "nanofactories";
+		nanofactoryDiv.appendChild(nanofactoryBuy);
+		nanofactoryDiv.appendChild(nanofactorySell);
+		nanofactoryDiv.appendChild(nanofactoryAmount);
+		document.getElementById('nanomachines').appendChild(nanofactoryDiv);
+		nanofactoryUnlock = false;
+	}
+	
+};
